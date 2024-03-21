4642be1df54a8ee6d7463a0721851fd3647675b6
nehyan9895 <muhammednehyan9895@gmail.com>
2024-03-21 23:05:57 +0530

    day-21 with question number 83

---
+++ Day-21/question-2.js
@@ -0,0 +1,30 @@
+// 83. Remove Duplicates from Sorted List
+
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once.
+// Return the linked list sorted as well.
+
+// Example 1:
+
+// Input: head = [1,1,2]
+// Output: [1,2]
+
+
+// Example 2:
+
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
+
+var deleteDuplicates = function(head) {
+    let curr = head;
+    while(curr&&curr.next){
+        if(curr.val==curr.next.val){
+            curr.next = curr.next.next
+        }else{
+            curr = curr.next
+        }
+    }
+    return head
+};
+
+
+
