8ad407127bf3846e75abec27ec2e0d7c5f158bee
nehyan9895 <muhammednehyan9895@gmail.com>
2024-04-02 17:53:01 +0530

    Day-33 with question number 205

---
+++ Day-33/question-1.js
@@ -0,0 +1,20 @@
+// 205. Isomorphic Strings
+
+// Given two strings s and t, determine if they are isomorphic.
+
+// Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+// All occurrences of a character must be replaced with another character while preserving the order of characters.
+// No two characters may map to the same character, but a character may map to itself.
+
+
+// Input: s = "egg", t = "add"
+// Output: true
+
+var isIsomorphic = function(s, t) {
+    if(s.length!== t.length) return false
+   for(let i = 0 ; i < s.length; i ++){
+       if(s.indexOf(s[i])!== t.indexOf(t[i])) return false
+   }
+   return true
+};
