c611fc2c260a575d475db73b19c864f43b240b50
nehyan9895 <muhammednehyan9895@gmail.com>
2024-04-05 23:13:01 +0530

    Day36 with question number 1544

---
+++ Day-35/question-1.js
@@ -0,0 +1,38 @@
+// 1614. Maximum Nesting Depth of the Parentheses
+
+
+// A string is a valid parentheses string (denoted VPS) if it meets one of the following:
+
+// It is an empty string "", or a single character not equal to "(" or ")",
+
+// It can be written as AB (A concatenated with B), where A and B are VPS's, or
+
+// It can be written as (A), where A is a VPS.
+
+// We can similarly define the nesting depth depth(S) of any VPS S as follows:
+
+
+// depth("") = 0
+// depth(C) = 0, where C is a string with a single character not equal to "(" or ")".
+// depth(A + B) = max(depth(A), depth(B)), where A and B are VPS's.
+// depth("(" + A + ")") = 1 + depth(A), where A is a VPS.
+
+// For example, "", "()()", and "()(()())" are VPS's (with nesting depths 0, 1, and 2), and ")(" and "(()" are not VPS's.
+
+// Given a VPS represented as string s, return the nesting depth of s.
+
+
+var maxDepth = function(s) {
+    let count = 0;
+    let maxNum = 0;
+    for (let c of s) {
+        if (c === '(') {
+            count++;
+            if (maxNum < count)
+                maxNum = count;
+        } else if (c === ')') {
+            count--;
+        }
+    }
+    return maxNum;
+};
---
+++ Day-36/question-1.js
@@ -0,0 +1,37 @@
+// 1544. Make The String Great
+
+// Given a string s of lower and upper case English letters.
+
+// A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+
+// 0 <= i <= s.length - 2
+// s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+
+// To make the string good, you can choose two adjacent characters that make the string bad and remove them.
+// You can keep doing this until the string becomes good.
+
+// Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+
+// Notice that an empty string is also good.
+
+
+var makeGood = function(s) {
+    const stack = [s[0]];
+     
+     for(let i = 1; i <= s.length - 1; i++){
+         if(stack.length > 0){
+             const a = s[i];
+             const b = stack[stack.length - 1];
+         if(a !== b && a.toLowerCase() === b.toLowerCase()){
+             stack.pop();
+         }else{
+             stack.push(s[i]);
+         }
+         }else{
+             stack.push(s[i]);
+         }
+     }
+     
+     return stack.join('');
+ };
+
