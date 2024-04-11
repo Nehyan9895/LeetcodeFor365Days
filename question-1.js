a34cf971e4e10c853ea6021783b9a1f9a7ce7f6c
nehyan9895 <muhammednehyan9895@gmail.com>
2024-04-11 23:19:45 +0530

    day-42 with question number 402

---
+++ question-1.js
@@ -0,0 +1,35 @@
+// 02. Remove K Digits
+
+// Given string num representing a non-negative integer num, and an integer k,
+// return the smallest possible integer after removing k digits from num.
+
+// Input: num = "1432219", k = 3
+// Output: "1219"
+// Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+
+
+var removeKdigits = function(num, k) {
+    if (num.length === k) return '0';
+
+   const stack = [];
+
+   for (let i = 0; i < num.length; i++) {
+       const current = num[i];
+       while (stack.length && stack[stack.length - 1] > current && k > 0 ) {
+           stack.pop();
+           k--;
+       }
+       stack.push(current);
+   }
+
+   while (k > 0) {
+       stack.pop();
+       k--;
+   }
+
+   while (stack.length > 1 && stack[0] === '0') {
+       stack.shift();
+   }
+
+   return stack.join('');
+};
